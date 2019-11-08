2306a7f327c0a774a53f82834c3c7b9cdac54fec
Itallo Henrique Castilho Castro <49693805+ItalloCastro09@users.noreply.github.com>
2019-11-08 19:11:36 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -14,15 +14,15 @@ const game = {
 
     ],
 
-    symbol: ['☥ ᘔ ᘗ Ƃ œ ᘴ ☧ ᘴ'],
-    symbol2: ['a b c d e f g h'],
-    symbol3: ['1 2 3 4 5 6 7 8'],
-    symbol4: ['9 8 7 6 5 4 3 2'],
-    symbol5: ['p o i u y t r e'],
-    symbol6: ['ç l k j h g f l'],
-    symbol7: ['m n b v c x z a'],
-    symbol8: ['p ç l o k m i j'],
-    senha: "BURACO NEGRO"
+    arrl: [''],
+    arr2: [''],
+    arr3: [''],
+    arr4: [''],
+    arr5: [''],
+    arr6: [''],
+    arr7: [''],
+    arr8: [''],
+    mainpass: ""
 }
 
 
