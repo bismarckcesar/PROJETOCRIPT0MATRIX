8c919d328da4e0a55605296eb54b9e80fd70060c
bismarckcesar <bismarcksport@gmail.com>
2019-11-08 20:50:03 -0300

    Update rotas.js

--- rotas.js
+++ rotas.js
@@ -11,8 +11,17 @@ routes.get('/', (req, res) => {
 routes.get('/jogo', (req, res) => {
     res.render('server', { dica: jogo.dica, arr: jogo.arr, senha: jogo.senha });
 });
+routes.get('/sobre', (req, res) => {
+    res.sendFile(__dirname + '/sobre.html');
+});
+routes.get('/ajuda', (req, res) => {
+    res.sendFile(__dirname + '/sobre.html');
+});
 routes.get('/ganhou', (req, res) => {
     res.sendFile(__dirname + '/ganhou.html');
+});
+routes.get('/perdeu', (req, res) => {
+    res.sendFile(__dirname + '/perdeu.html');
 });
 
 routes.post('/gabarito', (req, res) => {
