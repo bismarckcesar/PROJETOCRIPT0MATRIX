642599095d201f29ea5c4ec2955b16c09d1f81f3
Itallo Henrique Castilho Castro <49693805+ItalloCastro09@users.noreply.github.com>
2019-11-08 20:26:23 -0300

    Adicionado os input's

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('views', './views');
-app.set('view engine', 'pug');
+app.set('view engine', 'html');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 app.use(express.static('jogo'));
 app.use(express.static(__dirname));
 
-app.use(require('./routes'));
+app.use(require('rotas'));
 
 
 app.listen(8001);
