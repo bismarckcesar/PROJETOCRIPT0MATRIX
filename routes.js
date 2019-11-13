b63f28a24b5d0167deb8fe280d53ab8d1de52100
Itallo Henrique Castilho Castro <49693805+ItalloCastro09@users.noreply.github.com>
2019-11-13 16:56:56 -0300

    Níveis adicionados

--- routes.js
+++ routes.js
@@ -4,12 +4,56 @@ const routes = express.Router();
 
 
 routes.post("/gabarito", (req, res) => {
-    const resposta = ["incômodo", "pensador", "interior", "potássio", "assessor", "rousseau", "falecido", "lustrada", "embebido", "calmante", ]
+    const resposta = ["incômodo", "pensador", "interior", "potássio", "assessor", "rousseau", "falecido", "lustrada", "embebido", "calmante", "odisseia"]
     const submit = req.body
     const resultado = resposta.every((item, index) => item === submit[`p${index}`])
     console.log(resultado)
     if (resultado) {
-        res.sendFile(__dirname + '/ganhou.html');
+        res.sendFile(__dirname + '/public\fases\fase2.html');
+    } else {
+        res.sendFile(__dirname + '/perdeu.html');
+    }
+})
+routes.post("/gabarito2", (req, res) => {
+    const resposta = ["", "", "", "", "", "", "", "", "", "", ]
+    const submit = req.body
+    const resultado = resposta.every((item, index) => item === submit[`p${index}`])
+    console.log(resultado)
+    if (resultado) {
+        res.sendFile(__dirname + '/public\fases\fase3.html');
+    } else {
+        res.sendFile(__dirname + '/perdeu.html');
+    }
+})
+routes.post("/gabarito3", (req, res) => {
+    const resposta = ["", "", "", "", "", "", "", "", "", "", ]
+    const submit = req.body
+    const resultado = resposta.every((item, index) => item === submit[`p${index}`])
+    console.log(resultado)
+    if (resultado) {
+        res.sendFile(__dirname + '/public\fases\fase4.html');
+    } else {
+        res.sendFile(__dirname + '/perdeu.html');
+    }
+})
+routes.post("/gabarito4", (req, res) => {
+    const resposta = ["", "", "", "", "", "", "", "", "", "", ]
+    const submit = req.body
+    const resultado = resposta.every((item, index) => item === submit[`p${index}`])
+    console.log(resultado)
+    if (resultado) {
+        res.sendFile(__dirname + '/public\fases\bossfase.html');
+    } else {
+        res.sendFile(__dirname + '/perdeu.html');
+    }
+})
+routes.post("/gabarito5", (req, res) => {
+    const resposta = ["", "", "", "", "", "", "", "", "", "", ]
+    const submit = req.body
+    const resultado = resposta.every((item, index) => item === submit[`p${index}`])
+    console.log(resultado)
+    if (resultado) {
+        res.sendFile(__dirname + 'ganhou.html');
     } else {
         res.sendFile(__dirname + '/perdeu.html');
     }
