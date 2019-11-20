16e6274bd62bd7d996433a8b0d1bb64619abe4bf
Itallo Castro <49693805+ItalloCastro09@users.noreply.github.com>
2019-11-20 11:49:43 -0300

    Update jogo.js

--- Lixeira/jogo.js
+++ Lixeira/jogo.js
@@ -1,18 +1,24 @@
-const game = {
-    dica: [
-        'mal-estar', 0 'gabriel cantor', 1 'longe da metropole', 2 'elemento quimico', 3 'auxiliar', 4 'filosofo suiço', 5 'aquele que morreu', 6 'brilhante, encerada', 7
-    ],
-
-    arrl: ['☢ ☤ ☸ ♟ ☁ ☀ ☠ ♟'],
-    arr2: ['♬ ☂ ☤ ✡ ☏ ۩ ♟ ☆'],
-    arr3: ['☢ ☤ ☽ ☂ ☆ ☢ ♟ ☆'],
-    arr4: ['♬ ♟ ☽ ☏ ✡ ✡ ☢ ♟'],
-    arr5: ['☏ ✡ ✡ ☂ ✡ ✡ ♟ ☆'],
-    arr6: ['☆ ♟ ☯ ✡ ✡ ♟ ☏ ☯'],
-    arr7: ['♤ ☏ ❁ ☂ ☣ ☢ ✠ ♟'],
-    arr8: ['❁ ☯ ✡ ☽ ☆ ☏ ✠ ☏'],
-    mainpass: "Odisseia"
-}
-
-
-module.exports = game;
+♠ a
+♣ b
+☭ c
+♦ d
+♬ e
+♰ f
+♱ g
+� h
+Ø i
+& j
+✠ k
+✡ l
+✤ m
+✥ n 
+✪ o
+✐ p
+✖ q 
+☪ r
+☠ s
+☯ t
+♥ u
+♞ v
+☬ x 
+☮ z
